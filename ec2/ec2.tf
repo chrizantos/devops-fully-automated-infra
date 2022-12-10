10d1b2c722ccaaa230a2c006eab342006f5e8281
Chriz Angono <chrizantos.angono@gmail.com>
2022-12-10 16:01:04 -0500

    Second Updates

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -70,7 +70,7 @@ resource "aws_instance" "web_server" {
   tags                   = merge(var.tags, { Name = join("", [var.name, "-", "webserver"]) }, {Environment = var.name})
 
   # best practices as per checkov scanner
-
+  #########
   monitoring = true
   ebs_optimized = true
   root_block_device {
